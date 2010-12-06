6bba187e91a5420665a0a61248f930bcadab1cdb
wahyusumartha <wahyusumartha@gmail.com>
2010-12-06 21:28:54 +0800

    Update HomeScreen

--- src/com/framework/ui/screen/HomeScreen.java
+++ src/com/framework/ui/screen/HomeScreen.java
@@ -3,21 +3,21 @@ package com.framework.ui.screen;
 import com.framework.ui.component.CustomEditField;
 import com.framework.ui.preferences.FontPreference;
 
-import net.rim.device.api.ui.Font;
 import net.rim.device.api.ui.component.ButtonField;
-import net.rim.device.api.ui.container.HorizontalFieldManager;
 import net.rim.device.api.ui.container.MainScreen;
+import net.rim.device.api.ui.container.VerticalFieldManager;
 
 public class HomeScreen extends MainScreen {
 
 	private CustomEditField customEditField = new CustomEditField();
-	private FontPreference fontPreference;
 
 	public HomeScreen() {
-		HorizontalFieldManager horizontalFieldManager = new HorizontalFieldManager();
-		horizontalFieldManager.add(customEditField);
-		ButtonField button = new ButtonField();
-		horizontalFieldManager.add(button);
-		add(horizontalFieldManager);
+		VerticalFieldManager verticalFieldManager = new VerticalFieldManager();
+		ButtonField button = new ButtonField("Default Button");
+
+		verticalFieldManager.add(customEditField);
+		verticalFieldManager.add(button);
+
+		add(verticalFieldManager);
 	}
 }
