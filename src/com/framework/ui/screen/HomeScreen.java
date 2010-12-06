1da5804590c6581e226ab5841d44b18f802f30dc
wahyusumartha <wahyusumartha@gmail.com>
2010-12-06 22:43:31 +0800

    UPdate : Testing CustomLabelField in a screen

--- src/com/framework/ui/screen/HomeScreen.java
+++ src/com/framework/ui/screen/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.framework.ui.screen;
 
 import com.framework.ui.component.CustomEditField;
+import com.framework.ui.component.CustomLabelField;
 import com.framework.ui.preferences.FontPreference;
 
 import net.rim.device.api.ui.component.ButtonField;
@@ -10,6 +11,8 @@ import net.rim.device.api.ui.container.VerticalFieldManager;
 public class HomeScreen extends MainScreen {
 
 	private CustomEditField customEditField = new CustomEditField();
+	private CustomLabelField customLabelField = new CustomLabelField(
+			"Selamat Datang");
 
 	public HomeScreen() {
 		VerticalFieldManager verticalFieldManager = new VerticalFieldManager();
@@ -17,7 +20,7 @@ public class HomeScreen extends MainScreen {
 
 		verticalFieldManager.add(customEditField);
 		verticalFieldManager.add(button);
-
+		verticalFieldManager.add(customLabelField);
 		add(verticalFieldManager);
 	}
 }
